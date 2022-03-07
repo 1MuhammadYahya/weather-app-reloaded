77439c220cef82f00d0dbd0a174cae80d7d0c49a
warraich_sahab <yahoo.warraich@gmail.com>
2022-03-07 23:14:24 +0500

    Initialized Component and added some styling

---
+++ src/components/NavBar/Nav.svelte
@@ -0,0 +1,29 @@
+<script>
+	import SearchBar from './serach_bar.svelte';
+	import Logo from './logo.svelte';
+</script>
+
+<main>
+	<Logo/>
+	<SearchBar/>
+</main>
+
+<style>
+	main {
+      margin: 0;
+			width: 100vw;
+      position: absolute;
+			top: 0;
+      left: 0;
+			padding: 8px;
+			height: 36px;
+			background-color: var(--bg-color);
+			color: white;
+			box-shadow: var(--box-shadow);
+			display: flex;
+			justify-content: center;
+			align-items: center;
+	}
+
+	@media (min-width: 550px) { main { height: 24px; } }
+</style>
